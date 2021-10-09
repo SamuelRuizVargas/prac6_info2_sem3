9aa503f269699dc06bd66fbf8a0ebe92e8d9f750
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-10-09 13:53:11 -0500

    Actualizada la base para la clase planeta

--- planeta.cpp
+++ planeta.cpp
@@ -2,28 +2,112 @@
 
 planeta::planeta()
 {
+    tier=true;
     posx=0;
     posy=0;
+    vx=0;
+    vy=0;
+    masa=0;
+    radio=20;
     setPos(posx,posy);
 }
 
-planeta::planeta(int x, int y)
+planeta::planeta(int x, int y, int _vx, int _vy, int _masa, int _radio, string textura)
 {
     posx=x;
     posy=y;
+    vx=_vx;
+    vy=_vy;
+    masa=_masa;
+    radio=_radio;
     setPos(posx,posy);
+    if(textura=="sol")
+    {
+        sol=true;
+    }
+    else if(textura=="mercurio")
+    {
+        merc=true;
+    }
+    else if(textura=="venus")
+    {
+        venu=true;
+    }
+    else if(textura=="tierra")
+    {
+        tier=true;
+    }
+    else if(textura=="marte")
+    {
+        mart=true;
+    }
+    else if(textura=="jupiter")
+    {
+        jupi=true;
+    }
+    else if(textura=="saturno")
+    {
+        satu=true;
+    }
+    else if(textura=="urano")
+    {
+        uran=true;
+    }
+    else if(textura=="neptuno")
+    {
+        nept=true;
+    }
+    else if(textura=="NONE")
+    {
+        tier=true;
+    }
 }
 
 QRectF planeta::boundingRect() const
 {
-    return QRectF(0,0,20,20);
+    return QRectF(radio/2,radio/2,radio,radio);
 }
 
 void planeta::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    painter->setBrush(Qt::yellow);
-    painter->setPen(Qt::NoPen);
-    painter->drawEllipse(boundingRect());
+    QPixmap pixmap;
+    if(sol==true)
+    {
+        pixmap.load(PATH_SOL);
+    }
+    else if(merc==true)
+    {
+        pixmap.load(PATH_MERC);
+    }
+    else if(venu==true)
+    {
+        pixmap.load(PATH_VENU);
+    }
+    else if(tier==true)
+    {
+        pixmap.load(PATH_TIER);
+    }
+    else if(mart==true)
+    {
+        pixmap.load(PATH_MART);
+    }
+    else if(jupi==true)
+    {
+        pixmap.load(PATH_JUPI);
+    }
+    else if(satu==true)
+    {
+        pixmap.load(PATH_SATU);
+    }
+    else if(uran==true)
+    {
+        pixmap.load(PATH_URAN);
+    }
+    else if(nept==true)
+    {
+        pixmap.load(PATH_NEPT);
+    }
+    painter->drawPixmap(boundingRect(),pixmap,pixmap.rect());
 }
 
 planeta::~planeta()
--- planeta.h
+++ planeta.h
@@ -4,18 +4,42 @@
 #include <QGraphicsItem>
 #include <QPainter>
 
+#define PATH_SOL "../practica6/imagenes/sol.png"
+#define PATH_MERC "../practica6/imagenes/mercurio.png"
+#define PATH_VENU "../practica6/imagenes/venus.png"
+#define PATH_TIER "../practica6/imagenes/tierra.png"
+#define PATH_MART "../practica6/imagenes/marte.png"
+#define PATH_JUPI "../practica6/imagenes/jupiter.png"
+#define PATH_SATU "../practica6/imagenes/saturno.png"
+#define PATH_URAN "../practica6/imagenes/urano.png"
+#define PATH_NEPT "../practica6/imagenes/neptuno.png"
+
+using namespace std;
+
 class planeta : public QGraphicsItem
 {
-    int h=20;
-    int a=20;
+//    int h=20;
+//    int a=20;
     int posx;
     int posy;
-    int velocidad;
+    int vx;
+    int vy;
     int masa;
+    int radio;
+
+    bool sol=false;
+    bool merc=false;
+    bool venu=false;
+    bool tier=false;
+    bool mart=false;
+    bool jupi=false;
+    bool satu=false;
+    bool uran=false;
+    bool nept=false;
 
 public:
     planeta();
-    planeta(int,int);
+    planeta(int x,int y,int vx,int vy,int masa,int radio,string nom_planet);
     QRectF boundingRect() const;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
     ~planeta();
